5a3cba422394909371380c40c94c3beff4880cbc
mhowe <matthew.howe@kineticdata.com>
2022-09-29 17:18:02 +0900

    Added sql_generic_v4 handler
    These updates remove references to datastore, but changes info values

---
+++ handlers/sql_generic_v4/test/simple_input.rb
@@ -0,0 +1,25 @@
+{
+  'info' => {
+    'database_server' => '',
+	  'database_port' => '',
+    'database_username' => '',
+    'database_password' => '',
+    'kinetic_api_location' => '',
+    'kinetic_api_username' => '',
+    'kinetic_api_password' => '',
+    'space_form' => 'sql-query-template',
+    'enable_debug_logging' => 'Yes'
+  },
+  'parameters' => {
+    'jdbc_database' => 'postgresql',
+	  'dbname' => 'my_postgres',
+    'action'  => 'fetch',
+    'template_name' => 'Update Hero',
+    'query_values' => '{"secret_id":"superman","name":"clark kent"}',
+	
+    #'query'  => "UPDATE MKTest.dbo.test SET [First Name] = 'asdf' WHERE [Last Name] = 'Klein'",
+	  #'action'  => 'run',
+    
+	  'error_handling' => 'Raise Error'
+  }
+}
