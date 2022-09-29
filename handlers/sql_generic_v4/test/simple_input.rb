5e3d121ed6cc7a5311933d172fe5942512b91dc2
mhowe <matthew.howe@kineticdata.com>
2022-09-29 17:29:38 +0900

    Updated sql_generic_v4 simple_input.rb

--- handlers/sql_generic_v4/test/simple_input.rb
+++ handlers/sql_generic_v4/test/simple_input.rb
@@ -7,7 +7,8 @@
     'kinetic_api_location' => '',
     'kinetic_api_username' => '',
     'kinetic_api_password' => '',
-    'space_form' => 'sql-query-template',
+    'kapp_slug' => 'datastore',
+    'kapp_form_slug' => 'sql-query-template',
     'enable_debug_logging' => 'Yes'
   },
   'parameters' => {
@@ -16,10 +17,10 @@
     'action'  => 'fetch',
     'template_name' => 'Update Hero',
     'query_values' => '{"secret_id":"superman","name":"clark kent"}',
-	
+
     #'query'  => "UPDATE MKTest.dbo.test SET [First Name] = 'asdf' WHERE [Last Name] = 'Klein'",
 	  #'action'  => 'run',
-    
+
 	  'error_handling' => 'Raise Error'
   }
 }
